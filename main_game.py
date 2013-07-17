c46b17a249b8860e52008edee1273eb89a9cc3a2
Maciej Lis <mlisbit@gmail.com>
2013-07-17 00:06:18 -0400

    mouse tracking

--- main_game.py
+++ main_game.py
@@ -21,14 +21,19 @@ class TowerDefence(QtGui.QMainWindow):
 		if e.key() == QtCore.Qt.Key_P:
 			self.mainBoard.pause()
 
+	def mousePressEvent(self, e):
+		self.mainBoard.updateTowers()
+
 	def eventFilter(self, source, event):
 		if event.type() == QtCore.QEvent.MouseMove:
 			if event.buttons() == QtCore.Qt.NoButton and str(source).find("gameBoard") > 0:
  				pos = event.pos()
- 				print pos.x(), pos.y()
+ 				#print pos.x(), pos.y()
  				#self.edit.setText('x: %d, y: %d' % (pos.x(), pos.y()))
+ 				self.mainBoard.updateMouse(2, pos.x(), pos.y())
+ 				self.mainBoard.isMouseIn = True;
 			else:
-				pass # do other stuff
+				self.mainBoard.isMouseIn = False;
 		return QtGui.QMainWindow.eventFilter(self, source, event)
 	
 
@@ -36,8 +41,13 @@ class gameBoard(QtGui.QFrame):
 	boardWidth = 600
 	boardHeight = 520
 	blockSize = 20
-	boardOccupancy = [[0,0]]
-	startPosition = [0,0]
+
+	mouse_x = -1;
+	mouse_y = -1;
+	mouse_size = 1;
+
+	isMouseIn = False;
+	boardOccupancy = []
 
 	def start(self):
 		self.setStyleSheet("QWidget { background: #A9F5D0 }") 
@@ -48,6 +58,7 @@ class gameBoard(QtGui.QFrame):
 		qp.begin(self)
 		self.drawGrid(qp)
 		self.drawEnemies(qp)
+		self.drawOutline(qp)
 		
 		qp.end()
 
@@ -60,18 +71,31 @@ class gameBoard(QtGui.QFrame):
 
 
 	def drawEnemies(self, qp):
-		qp.setPen(QtCore.Qt.NoPen)
-		qp.setBrush(QtGui.QColor(255, 80, 0, 255))
-		qp.drawRect(0, 0, 40, 40)
-		qp.setPen(QtCore.Qt.NoPen)
-		qp.setBrush(QtGui.QColor(225, 180, 40, 255))
-		qp.drawRect(0, 0, 20, 20)
+		pass
 
 	def drawPath(self, qp):
 		pass
 
 	def pause(self):
 		print "paused"
+
+	def drawOutline(self, qp):
+		qp.setPen(QtCore.Qt.NoPen)
+		qp.setBrush(QtGui.QColor(255, 80, 0, 155))
+		qp.drawRect(self.myround(self.mouse_x), self.myround(self.mouse_y), self.mouse_size*20, self.mouse_size*20)
+
+	def myround(self, x, base=20):
+		#return int(base * round(float(x)/base)) 
+		return x - (x%base)
+
+	def updateMouse(self, size, x, y):
+		self.mouse_size = size
+		self.mouse_y = y
+		self.mouse_x = x
+		print x, y
+		self.repaint()
+	def updateTowers(self):
+		print self.isMouseIn
 
 class scoreBoard(QtGui.QFrame):
 	'''
